49cfd6d6f9e981331f6b666b558beee3d6bf0839
Paweł Rubin <pawelrubin19@gmail.com>
2021-06-17 22:54:40 +0200

    Fixes

--- plot.py
+++ plot.py
@@ -7,13 +7,12 @@ data = pd.DataFrame(data)
 pos = data[0]
 neg = data[1]
 
-plt.plot(pos, label="+1 ")
-plt.plot(neg, label=" -1")
-plt.xlabel(" Repetitions ")
-plt.ylabel(" Distribution of states ")
+plt.plot(pos, label="+1")
+plt.plot(neg, label="-1")
+plt.xlabel("Iterations")
+plt.ylabel("Distribution")
 plt.legend()
 plt.show()
-
 
 data = pd.read_csv("trajectory.txt", sep="\s+", header=None)
 data = pd.DataFrame(data)
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,20 @@ use rand;
 use std::fs::File;
 use std::io::Write;
 
-const MIN_RUNS: u64 = 1_000_000;
+const MIN_RUNS: u64 = 1_000;
 const ALPHA: f32 = 0.45;
 const BETA: f32 = 0.5;
 
-fn random_walk(alpha: f32, beta: f32, epsilon: f32) -> Vec<(f32, f32)> {
+fn random_walk(alpha: f32, beta: f32, epsilon: f32) -> Vec<f32> {
     let mut state = true;
     let mut pos_states = 0;
-
-    let mut distribution: Vec<(f32, f32)> = vec![(0.0, 0.0)];
-
-    let mut last_pos = 0.0;
-    let mut last_neg = 0.0;
+    let mut distribution: Vec<f32> = vec![0.0];
+    let mut last: f32 = 0.0;
+    let mut next: f32 = f32::MAX;
     let mut i: u64 = 0;
-    loop {
+    while i < MIN_RUNS || (next - last).abs() >= epsilon {
         i += 1;
+
         if state {
             pos_states += 1;
             if rand::random::<f32>() < alpha {
@@ -27,20 +26,12 @@ fn random_walk(alpha: f32, beta: f32, epsilon: f32) -> Vec<(f32, f32)> {
                 state = true;
             }
         }
-        let next_pos = pos_states as f32 / i as f32;
-        let next_neg = 1.0 - (pos_states as f32 / i as f32);
-
-        distribution.push((next_pos, next_neg));
-
-        if i >= MIN_RUNS
-            && (next_pos - last_pos).abs() < epsilon
-            && (next_neg - last_neg).abs() < epsilon
-        {
-            return distribution;
-        }
-        last_pos = next_pos;
-        last_neg = next_neg;
+        last = next;
+        next = pos_states as f32 / i as f32;
+        distribution.push(next);
     }
+    println!("total iterations = {}", i);
+    return distribution;
 }
 
 fn trajectory(alpha: f32, beta: f32, reps: u32) -> Vec<bool> {
@@ -59,13 +50,23 @@ fn trajectory(alpha: f32, beta: f32, reps: u32) -> Vec<bool> {
 }
 
 fn main() {
-    let mut output_file = File::create("result.txt").unwrap();
-    random_walk(ALPHA, BETA, 1.0e-6)
-        .into_iter()
-        .for_each(|(p, n)| writeln!(output_file, "{}", format!("{} {}", p, n)).unwrap());
+    writeln!(
+        File::create("result.txt").unwrap(),
+        "{}",
+        random_walk(ALPHA, BETA, f32::EPSILON)
+            .iter()
+            .map(|n| format!("{} {}", n, 1.0 - n))
+            .collect::<Vec<String>>()
+            .join("\n")
+    ).unwrap();
 
-    output_file = File::create("trajectory.txt").unwrap();
-    trajectory(ALPHA, BETA, 200).into_iter().for_each(|state| {
-        writeln!(output_file, "{}", format!("{}", if state { 1 } else { -1 })).unwrap()
-    });
+    writeln!(
+        File::create("trajectory.txt").unwrap(),
+        "{}",
+        trajectory(ALPHA, BETA, 200)
+            .into_iter()
+            .map(|state| format!("{}", if state { 1 } else { -1 }))
+            .collect::<Vec<String>>()
+            .join("\n")
+    ).unwrap();
 }
